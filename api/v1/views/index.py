7c0c6e633e39d6068f2441eb70dadd3a44a1fe84
Chiemezie Jacob <chiemezie.jacob@gmail.com>
2023-01-23 15:03:53 -0500

    rectified and documented the api folder modules

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+"""Connecting to the api using flask functions"""
 from api.v1.views import app_views
 
 @app_views.route('/status')
 def status():
-    """status"""
+    """hbnb status"""
     return (jsonify({"status": "OK"}))
