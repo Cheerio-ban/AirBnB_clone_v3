1cf38f83d8eb6dca50a733f609947fa2d312610c
Chiemezie Jacob <chiemezie.jacob@gmail.com>
2023-01-23 20:08:06 -0500

    error handler

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Contains route methods"""
 
-from flask import Flask
+from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -15,6 +15,11 @@ app.register_blueprint(app_views)
 def teardown_appcontext(error):
     """Tears down context"""
     storage.close()
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 
 if __name__ == '__main__':
