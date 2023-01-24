438ab8a7c889a0faa3d39575a2ac89f5cda93f74
Chiemezie Jacob <chiemezie.jacob@gmail.com>
2023-01-23 20:45:33 -0500

    User model

--- models/user.py
+++ models/user.py
@@ -20,10 +20,19 @@ class User(BaseModel, Base):
         reviews = relationship("Review", backref="user")
     else:
         email = ""
-        password = ""
+        __password = ""
         first_name = ""
         last_name = ""
 
     def __init__(self, *args, **kwargs):
         """initializes user"""
         super().__init__(*args, **kwargs)
+
+    @property
+    def password(self):
+        return self._password
+
+    @password.setter
+    def password(self, pwd):
+        """hashing password values"""
+        self._password = hashlib.md5(pwd.encode()).hexdigest()
